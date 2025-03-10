f474fef0b0aad8a57836ddaeec686b8294aa6489
Tornao2 <szymon.zuber2003@gmail.com>
2025-03-10 21:35:15 +0100

    Reworked how mainMenu initiation works

--- Engine.cpp
+++ Engine.cpp
@@ -17,25 +17,30 @@ int Engine::mainLoop() {
 }
 
 bool Engine::initMainMenu() {
-    sf::Texture* mainMenuTexture = spriteHandler->loadTexture({ 320, 320 }, "MainMenu");
-    if (!mainMenuTexture) return true;
-    for (int y = 0; y < 4; y++) {
-        usedTextures.emplace_back(spriteHandler->loadSprite(*mainMenuTexture, { 320, 80 }, { 0, y * 80 }));
-        usedTextures[y].setPosition({ 240, y * 120.0f + 40 });
+    sf::Texture* mainMenuTexture = spriteHandler->loadTexture({ 400, 600 }, "MainMenu");
+    if (!mainMenuTexture) return true;  
+    for (int i = 0; i < 4; i++) {
+        usedTextures.emplace_back(spriteHandler->loadSprite(*mainMenuTexture, { 400,100 }, { 0, 100 }));
+        usedTextures.back().setPosition({200, 50.0f + 125.0f * i});
     }
+    for (int i = 0; i < 4; i++) {
+        usedTextures.emplace_back(spriteHandler->loadSprite(*mainMenuTexture, { 400, 100 }, { 0, 200 + i * 100 }));
+        usedTextures.back().setPosition({ 200, 50.0f + 125.0f * i });
+    }
+    usedTextures.emplace_back(spriteHandler->loadSprite(*mainMenuTexture, { 400,100 }, { 0, 0 }));
+    usedTextures.back().setPosition({ 200, 50. });
     return false;
 }
 
 void Engine::draw() {
     display->getWindow()->clear();
-    for (sf::Sprite sprite : usedTextures) 
+    for (sf::Sprite sprite : usedTextures)
         display->getWindow()->draw(sprite);
     display->getWindow()->display();
 }
 
 void Engine::handleEvents() {
-    while (std::optional event = display->getWindow()->pollEvent())
-    {
+    while (std::optional event = display->getWindow()->pollEvent()){
         if (event->is<sf::Event::Closed>()) 
             display->getWindow()->close();
     }
